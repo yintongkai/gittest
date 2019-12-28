b42b2ca161df4d454368fac431246ac78a942a1d
yintongkai <291650353@qq.com>
2019-12-29 01:00:52 +0800

    测试git

---
+++ src/main/java/com/alibaba/HelloWord.java
@@ -0,0 +1,13 @@
+package com.alibaba;
+
+/**
+ * @program: gittest
+ * @description: none
+ * @author: tongkai yin
+ * @create: 2019/12/29 00:48
+ */
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.printf("%s","Hello World");;
+    }
+}
